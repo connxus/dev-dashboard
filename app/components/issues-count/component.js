19a559fd9f5bf182da8b823587a845a8a9279007
Paul Schoen <paul@connxus.com>
2019-10-07 14:37:50 -0400

    Graphs

--- app/components/issues-count/component.js
+++ app/components/issues-count/component.js
@@ -1,9 +1,29 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  data: null,
-  title: null,
+  appIssues: null,
+  appTitles: null,
+  apiIssues: null,
+  apiTitle: null,
   ajax: Ember.inject.service(),
+  appChartData: {
+    labels: Ember.A(),
+    datasets: Ember.A()
+  },
+
+  options: {
+    scales: {
+      xAxes: [{ stacked: true }],
+      yAxes: [{ stacked: true }]
+    },
+    legend: {
+      labels: {
+        fontColor: 'white',
+        defaultFontSize: 24,
+        defaultFontStyle: 'bold'
+      }
+    }
+  },
 
   didInsertElement(){
     this.send('getAppData');
@@ -12,27 +32,49 @@ export default Ember.Component.extend({
   actions: {
     getAppData() {
       var self = this;
-      return this.get('ajax').request('/cxs-app', {method: 'GET'}).then(function(response) {
+      let appChartDataArr = Ember.A();
+
+      this.get('ajax').request('/cxs-app', {method: 'GET'}).then(function(response) {
         if (response.data) {
-          self.set('data', response.data);
-          self.set('title', response.title);
-        }
-        setTimeout(function() {
-          self.send('getApiData');
-        }, 30000);
+          self.set('appTitle', response.data.map(issue => issue.name));
+          self.set('appIssues', response.data.map(issue => issue.issueLength));
+        };
+
+        self.set('appChartData.labels', self.get('appTitle'));
+
+        appChartDataArr.push({
+          label: 'App Tickets',
+          data: self.get('appIssues'),
+          backgroundColor: 'rgb(65,149,238)'
+        });
+
+        Ember.run.later(() => {
+          self.set('appChartData.datasets', appChartDataArr);        
+        },5000)
       });
-    },
-    getApiData() {
-      var self = this;
-      return this.get('ajax').request('/cxs-api', {method: 'GET'}).then(function(response) {
+
+      this.get('ajax').request('/cxs-api', {method: 'GET'}).then(function(response) {
         if (response.data) {
-          self.set('data', response.data);
-          self.set('title', response.title);
-        }
-        setTimeout(function() {
-          self.send('getAppData');
-        }, 30000);
+          console.log(response.data)
+          self.set('apiTitle', response.data.map(issue => issue.name));
+          self.set('apiIssues', response.data.map(issue => issue.issueLength));
+        };
+
+        appChartDataArr.push({
+          label: 'Api Tickets',
+          data: self.get('apiIssues'),
+          backgroundColor: 'rgb(68,218,116)'
+        });
+
+        Ember.run.later(() => {
+          self.set('appChartData.datasets', appChartDataArr);          
+        },5000)
       });
+
+      setTimeout(function() {
+        this.set('appChartData.datasets', Ember.A());
+        self.send('getAppData');
+      }, 30000);
     }
   }
 });
